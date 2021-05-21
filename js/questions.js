b4a1b39942001d9fba87024970ddfc023777f78e
vjapp3 <56712854+vjapp3@users.noreply.github.com>
2021-05-21 14:25:08 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is a correct syntax to output Hello World in Java?",
+    answer: "System.out.println(Hello World);",
+    options: [
+      "echo(Hello World);",
+      "print(Hello World);",
+      "Console.WriteLine(Hello World);",
+      "System.out.println(Hello World);"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the correct way to create an object called myObj of MyClass?",
+    answer: "MyClass myObj = new MyClass();",
+    options: [
+      "MyClass myObj = new MyClass;",
+      "MyClass myObj = new Myclass();",
+      "MyClass myObj = new MyClass();",
+      "MyClass myobj = new MyClass()"
+    ]
+  },
+    {
+    numb: 6,
+    question: "How do you create a variable with the floating number 2.8?",
+    answer: "float x = 2.8f;",
+    options: [
+      "int x = 2.8f",
+      "byte x= 2.8f",
+      "x = 2.8f;",
+      "float x = 2.8f;"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 8,
+    question: "To declare an array in Java, define the variable type with:",
+    answer: "[]",
+    options: [
+      "{}",
+      "()",
+      "[]",
+      "<>"
+    ]
+  },
+    {
+    numb: 9,
+    question: "How do you insert COMMENTS in Java code?",
+    answer: "//This is a comment",
+    options: [
+      "//This is a comment",
+      "/=This is a comment",
+      "/This is a comment",
+      "/*/This is a comment"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Java is short for JavaScript",
+    answer: "False",
+    options: [
+      "True",
+      "False", 
+      "MayBe",
+      "Non Of The Above",   ]
+  },
+  
+];
